b9c6e63c6ece3ea0bfec4cac5ed8e0372f67337d
Oleg Demchenko <d.oleg-19953@mail.ru>
2023-10-31 17:52:02 +0300

    added typescript and included the material ui library

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TextFields from "./components/TextField/TextField";
+
+function App() {
+
+  return (
+    <div>
+      <TextFields/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { TextField } from "@mui/material";
+import "./TextField.css"
+
+export default function TextFields() {
+  return (
+      <TextField className={"input"} type={"string"} value={"jjjjj"} />
+  );
+}
