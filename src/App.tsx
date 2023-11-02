a1264ce58988b2b62f710c9fc4c9e36a8c6e71cf
Oleg Demchenko <d.oleg-19953@mail.ru>
2023-11-02 17:19:20 +0300

    added a button to generate all fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useMemo, useState } from "react";
 import { BaseInput } from "./components/BaseInput/BaseInput";
 import { INPUT_LABELS } from "./constants";
 import { password } from "./helpers/password";
@@ -6,9 +7,9 @@ import { transliterator } from "./helpers/namesTranslit";
 import { generateRussianPhoneNumber } from "./helpers/phoneNumber";
 import { femaleNames } from "./dataNames/femaleNames";
 import { generateEmail } from "./helpers/emailAddress";
-import { useMemo } from "react";
 import { maleNames } from "./dataNames/maleNames";
 import { TAXGenerator } from "./helpers/TAXGenerator";
+import { BaseButton } from "./components/Button/Button";
 
 function App() {
   const { phone_number, firstname, lastname, middlename, email_address, inn } = INPUT_LABELS;
@@ -23,15 +24,40 @@ function App() {
     }
   }, [randomGender, randomIndex])
 
+  const [phoneNumber, setPhoneNumber] = useState(generateRussianPhoneNumber());
+  const [email, setEmail] = useState(generateEmail());
+  const [pass, setPass] = useState(password);
+  const [lastnames, setLastnames] = useState(transliterator(getNames.lastname));
+  const [firstnames, setFirstnames] = useState(transliterator(getNames.firstname));
+  const [middlenames, setMiddlenames] = useState(transliterator(getNames.middlename));
+  const [inns, setInns] = useState(TAXGenerator());
+
+  const getStarted = () => {
+    setPhoneNumber(generateRussianPhoneNumber());
+    setEmail(generateEmail());
+    setLastnames(transliterator(getNames.lastname));
+    setFirstnames(transliterator(getNames.firstname));
+    setMiddlenames(transliterator(getNames.middlename));
+    setInns(TAXGenerator());
+  }
+
   return (
     <div>
-      <BaseInput label={phone_number} value={generateRussianPhoneNumber()}/>
-      <BaseInput label={email_address} value={generateEmail()}/>
-      <BaseInput label={INPUT_LABELS.password} value={password}/>
-      <BaseInput label={lastname} value={transliterator(getNames.lastname)}/>
-      <BaseInput label={firstname} value={transliterator(getNames.firstname)}/>
-      <BaseInput label={middlename} value={transliterator(getNames.middlename)}/>
-      <BaseInput label={inn} value={TAXGenerator()}/>
+      <BaseButton onClick={() => getStarted()}/>
+      <BaseInput label={phone_number}
+                 value={phoneNumber}/>
+      <BaseInput label={email_address}
+                 value={email}/>
+      <BaseInput label={INPUT_LABELS.password}
+                 value={pass}/>
+      <BaseInput label={lastname}
+                 value={lastnames}/>
+      <BaseInput label={firstname}
+                 value={firstnames}/>
+      <BaseInput label={middlename}
+                 value={middlenames}/>
+      <BaseInput label={inn}
+                 value={inns}/>
     </div>
   )
 }
